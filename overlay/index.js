574e2f6a388b88db8d7003ad435e15c9be7868e9
Toby N <jakeypaulyguyy@gmail.com>
2020-10-27 16:43:20 -0400

    added overlay

---
+++ overlay/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+
+const app = express();
+const port = 80;
+
+// Configure express to user body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors())
+
+// For every file ending in .html in the html folder send the file to the user loading the page
+app.get('/*.html?', function (req,res) {
+    res.sendFile(req.url, {root: './html'});
+});
+
+var x;
+var y;
+var z;
+
+app.post('/send-coords', (req, res) => {
+    x = req.body.x;
+    y = req.body.y;
+    z = req.body.z;
+    res.sendStatus(200);
+});
+
+app.post('/get-coords', (req, res) => {
+    res.send({x: x, y: y, z: z});
+});
+
+app.listen(port, () => console.log(`App listening at port ${port}`));
