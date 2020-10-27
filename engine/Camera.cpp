e55c367d87c4f0cf98b64484e10c2afa5b008c61
Toby N <jakeypaulyguyy@gmail.com>
2020-10-27 16:43:06 -0400

    fixed camera sdk functions

--- engine/Camera.cpp
+++ engine/Camera.cpp
@@ -9,59 +9,38 @@
 
 namespace Engine
 {
-	Vector3& Camera::GetViewRight()
+	Vector3 Camera::GetViewRight()
 	{
-		if (!Memory::IsValidPtr<Camera>(this))
-			return *(new Vector3());
-
-		return *Memory::Ptr<Vector3*>(this, OFFSET_CAMERA_VIEWRIGHT);
+		return proc.Read<Vector3>((uintptr_t)this + OFFSET_CAMERA_VIEWRIGHT);
 	}
 
-	Vector3& Camera::GetViewUp()
+	Vector3 Camera::GetViewUp()
 	{
-		if (!Memory::IsValidPtr<Camera>(this))
-			return *(new Vector3());
-
-		return *Memory::Ptr<Vector3*>(this, OFFSET_CAMERA_VIEWUP);
+		return proc.Read<Vector3>((uintptr_t)this + OFFSET_CAMERA_VIEWUP);
 	}
 
-	Vector3& Camera::GetViewForward()
+	Vector3 Camera::GetViewForward()
 	{
-		if (!Memory::IsValidPtr<Camera>(this))
-			return *(new Vector3());
-
-		return *Memory::Ptr<Vector3*>(this, OFFSET_CAMERA_VIEFORWARD);
+		return proc.Read<Vector3>((uintptr_t)this + OFFSET_CAMERA_VIEFORWARD);
 	}
 
-	Vector3& Camera::GetViewTranslation()
+	Vector3 Camera::GetViewTranslation()
 	{
-		if (!Memory::IsValidPtr<Camera>(this))
-			return *(new Vector3());
-
-		return *Memory::Ptr<Vector3*>(this, OFFSET_CAMERA_VIEWTRANSLATION);
+		return proc.Read<Vector3>((uintptr_t)this + OFFSET_CAMERA_VIEWTRANSLATION);
 	}
 
 	float Camera::GetViewFovX()
 	{
-		if (!Memory::IsValidPtr<Camera>(this))
-			return 0.0f;
-
-		return *Memory::Ptr<float*>(this, OFFSET_CAMERA_VIEWFOVX);
+		return proc.Read<float>((uintptr_t)this + OFFSET_CAMERA_VIEWFOVX);
 	}
 
 	float Camera::GetViewFovY()
 	{
-		if (!Memory::IsValidPtr<Camera>(this))
-			return 0.0f;
-
-		return *Memory::Ptr<float*>(this, OFFSET_CAMERA_VIEWFOVY);
+		return proc.Read<float>((uintptr_t)this + OFFSET_CAMERA_VIEWFOVY);
 	}
 
 	Vector3 Camera::WorldToScreen(Vector3& position)
 	{
-		if (!Memory::IsValidPtr<Camera>(this))
-			return *(new Vector3());
-
 		Vector3 temp = position - GetViewTranslation();
 
 		float x = temp.Dot(GetViewRight());
